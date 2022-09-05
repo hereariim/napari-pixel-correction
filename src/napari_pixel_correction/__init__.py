4e454774c383a28fb5cabf3cd284fdc36645db22
hereariim <herearii.metuarea@gmail.com>
2022-09-05 17:09:18 +0200

    lk

--- src/napari_pixel_correction/__init__.py
+++ src/napari_pixel_correction/__init__.py
@@ -1,12 +1,11 @@
 __version__ = "0.0.1"
 
-from ._widget import process_function_load, save_modification, save_as_zip
+from ._reader import napari_get_reader
+from ._widget import process_function_load
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
     "write_single_image",
     "write_multiple",
     "process_function_load",
-    "save_modification",
-    "save_as_zip",
 )
--- src/napari_pixel_correction/_tests/test_widget.py
+++ src/napari_pixel_correction/_tests/test_widget.py
@@ -1,9 +1,3 @@
 
 def test_process_function_load():
     pass
-
-def test_save_modification():
-    pass
-
-def test_save_as_zip():
-    pass
