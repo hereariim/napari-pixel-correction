3a42454f2c37595947e0f1ffc24ff64f0ebbb664
hereariim <herearii.metuarea@gmail.com>
2022-09-21 09:47:32 +0200

    5

--- src/napari_pixel_correction/__init__.py
+++ src/napari_pixel_correction/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.1"
 
 from ._reader import napari_get_reader
-from ._widget import process_function_load
+from ._widget import process_function_load,save_modification,save_as_zip
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
